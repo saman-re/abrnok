cb7876b55202e4f0005449dba92dc84da385c959
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-13 01:24:13 +0330

    choose server section added to new_server page

---
+++ src/components/New Server/ChooseServer.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row class="mx-4 mt-7">
+    <v-col cols="12" class="display-1"> Choose Server </v-col>
+    <v-col
+      cols="12"
+      lg="3"
+      md="4"
+      sm="6"
+      v-for="(server, index) in servers"
+      :key="index"
+    >
+    <app-server-card :server="server"></app-server-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import ServerCard from './ServerCard';
+export default {
+  data() {
+    return {
+      servers: [
+        {
+          title: "Cloud Compute",
+        },
+        {
+          title: "High Frequency",
+        },
+        {
+          title: "Bare Metal",
+        },
+        {
+          title: "Dedicated Cloud ",
+        },
+      ],
+    };
+  },
+  components:{
+      'app-server-card':ServerCard
+  }
+};
+</script>
---
+++ src/components/New Server/ServerCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card
+        flat
+        :elevation="cardEval"
+        @mousemove="cardEval = 15"
+        @mouseleave="cardEval = 2"
+        style="cursor:pointer"
+      >
+        <div class="blue" style="height:120px"></div>
+        <v-card-title>
+          {{ server.title }}
+        </v-card-title>
+      </v-card>
+</template>
+
+<script>
+export default {
+    props:["server"],
+    data(){
+        return{
+            cardEval: 2,
+        }
+    }
+}
+</script>
