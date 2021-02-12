f4b2992e7b45660dc628e487b72c498486635417
saman-re <saman.rezapoor1379@yahoo.com>
2021-02-13 01:53:06 +0330

    navigation drawer fixed

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,14 +1,14 @@
 <template>
-    <v-navigation-drawer app v-model="navDrawer" clipped width="130px" class="blue darken-3 container">
+    <v-navigation-drawer app v-model="navDrawer" clipped width="90px" class="blue darken-3 container px-0">
       <v-list>
-        <v-list-item class="pa-0 container" v-for="(listItem,index) in listItems" :key="index">
-          <v-card flat class="blue darken-3">
+        <v-list-item class="container px-0" v-for="(listItem,index) in listItems" :key="index">
+          <v-card flat width="100%" class="blue darken-3">
             <v-card-text class="text-center pb-0">
             <v-icon x-large class="black--text">
               mdi-{{listItem.icon}}
             </v-icon>
             </v-card-text>
-            <v-card-subtitle class="pt-0">
+            <v-card-subtitle class="pt-0 text-center">
               {{listItem.subtitle}}
             </v-card-subtitle>
           </v-card>
